0a092370100585246dfac7271e0b752f84f671b8
strangehood <55440847+strangehood@users.noreply.github.com>
2019-10-22 13:58:53 +0300

    Nick.v3.1

--- gun.py
+++ gun.py
@@ -13,71 +13,72 @@ canv.pack(fill=tk.BOTH, expand=1)
 
 
 class ball():
-	def __init__(self, x=40, y=450):
-		""" Конструктор класса ball
+    def __init__(self, x=40, y=450):
+        """ Конструктор класса ball
 
 		Args:
 		x - начальное положение мяча по горизонтали
 
 		y - начальное положение мяча по вертикали
 		"""
-		self.x = x
-		self.y = y
-		self.r = 15
-		self.vx = 0
-		self.vy = 0
-		self.color = choice(['blue', 'green', 'red', 'brown'])
-		self.id = canv.create_oval(
-				self.x - self.r,
-				self.y - self.r,
-				self.x + self.r,
-				self.y + self.r,
-				fill=self.color)
+        self.x = x
+        self.y = y
+        self.r = 15
+        self.vx = 0
+        self.vy = 0
+        self.color = choice(['blue', 'green', 'red', 'brown'])
+        self.id = canv.create_oval(
+            self.x - self.r,
+            self.y - self.r,
+            self.x + self.r,
+            self.y + self.r,
+            fill=self.color)
 
-		self.live = 30
+        self.live = 30
 
-	def set_coords(self):
-		canv.coords(
-				self.id,
-				self.x - self.r,
-				self.y - self.r,
-				self.x + self.r,
-				self.y + self.r
+    def set_coords(self):
+        canv.coords(
+            self.id,
+            self.x - self.r,
+            self.y - self.r,
+            self.x + self.r,
+            self.y + self.r
         )
 
-	def move(self):
-		"""Переместить мяч по прошествии единицы времени.
+    def move(self):
+        """Переместить мяч по прошествии единицы времени.
 
 		Метод описывает перемещение мяча за один кадр перерисовки. То есть, обновляет значения
 		self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации, действующей на мяч,
 		и стен по краям окна (размер окна 800х600).
 		"""
         # FIXME
-		canv.move (self.id, self.vx, self.vy)
-		self.x += self.vx
-		self.y -= self.vy
+        canv.move(self.id, self.vx, self.vy)
+        self.x += self.vx
+        self.y -= self.vy
 
-		if ((self.x < 0) or (self.x > 800)):
-			self.vx = -self.vx
+        if ((self.x < 0) or (self.x > 800)):
+            self.vx = -self.vx
 
-		if ((self.y < 0) or (self.y > 600)):
-			self.vy = -self.vy
-		self.vy += 1
+        if ((self.y < 0) or (self.y > 600)):
+            self.vy = -self.vy
+        self.vy += 1
 
-	def hittest(self, obj):
-		"""Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
+    def hittest(self, obj):
+        """Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
 
 		Args:
 			obj: Обьект, с которым проверяется столкновение.
 		Returns:
 			Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
 		"""
-		if (((canv.coords(obj.id)[0] - obj.r + - self.x)**2 + (canv.coords(obj.id)[1] - obj.r - self.y)**2) <= (self.r + obj.r)**2):
-			return True
+        if (((canv.coords(obj.id)[0] - obj.r + - self.x) ** 2 + (canv.coords(obj.id)[1] - obj.r - self.y) ** 2) <= (
+                self.r + obj.r) ** 2):
+            return True
 
         # FIXME
-		else:
-			return False
+        else:
+            return False
 
 
 class gun():
@@ -105,8 +106,8 @@ class gun():
             self.angle = math.pi / 2
         else:
             self.angle = - math.pi / 2
-        new_ball.vx = self.f2_power * math.cos(self.an)
-        new_ball.vy = self.f2_power * math.sin(self.an)
+        new_ball.vx = self.f2_power * math.cos(self.angle)
+        new_ball.vy = self.f2_power * math.sin(self.angle)
         balls += [new_ball]
         self.f2_on = 0
         self.f2_power = 10
@@ -114,7 +115,7 @@ class gun():
     def targetting(self, event=0):
         """Прицеливание. Зависит от положения мыши."""
         if event:
-            self.an = math.atan((event.y - 450) / (event.x - 20))
+            self.angle = math.atan((event.y - 450) / (event.x - 20))
         if self.f2_on:
             canv.itemconfig(self.id, fill='orange')
         else:
@@ -132,28 +133,29 @@ class gun():
         else:
             canv.itemconfig(self.id, fill='black')
 
+
 class target():
-	def __init__(self):
-		self.points = 0
-		self.live = 1
-		self.id = canv.create_oval(0, 0, 0, 0)
-		self.id_points = canv.create_text(30, 30, text=self.points, font='28')
-		self.new_target()
+    def __init__(self):
+        self.points = 0
+        self.live = 1
+        self.id = canv.create_oval(0, 0, 0, 0)
+        self.id_points = canv.create_text(30, 30, text=self.points, font='28')
+        self.new_target()
 
-	def new_target(self):
-		""" Инициализация новой цели. """
-		x = self.x = rnd(600, 780)
-		y = self.y = rnd(300, 550)
-		r = self.r = rnd(2, 50)
-		color = self.color = 'red'
-		canv.coords(self.id, x-r, y-r, x+r, y+r)
-		canv.itemconfig(self.id, fill=color)
+    def new_target(self):
+        """ Инициализация новой цели. """
+        x = self.x = rnd(600, 780)
+        y = self.y = rnd(300, 550)
+        r = self.r = rnd(2, 50)
+        color = self.color = 'red'
+        canv.coords(self.id, x - r, y - r, x + r, y + r)
+        canv.itemconfig(self.id, fill=color)
 
-	def hit(self, points=1):
-		"""Попадание шарика в цель."""
-		canv.coords(self.id, -10, -10, -10, -10)
-		self.points += points
-		canv.itemconfig(self.id_points, text=self.points)
+    def hit(self, points=1):
+        """Попадание шарика в цель."""
+        canv.coords(self.id, -10, -10, -10, -10)
+        self.points += points
+        canv.itemconfig(self.id_points, text=self.points)
 
 
 t1 = target()
@@ -195,4 +197,3 @@ def new_game(event=''):
 new_game()
 
 mainloop()
-
